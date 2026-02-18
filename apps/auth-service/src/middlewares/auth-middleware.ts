197b6721a46e5aa4ed181f422ec72c141b51e0a1
Ashminita <ashminitab@mindfiresolutions.com>
2026-02-18 18:25:06 +0530

    fix: auth middleware

--- apps/auth-service/src/middlewares/auth-middleware.ts
+++ apps/auth-service/src/middlewares/auth-middleware.ts
@@ -1,6 +1,7 @@
 import { NextFunction, Request, Response } from "express";
 import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
+import { verifyAccessToken } from "../utils/jwt";
 
 dotenv.config();
 
@@ -15,24 +16,13 @@ export const authenticate = (
 	res: Response,
 	next: NextFunction,
 ) => {
-	const authHeader = req.headers.authorization;
-
-	if (!authHeader || !authHeader.startsWith("Bearer")) {
-		return res.status(401).json({
-			message: "Access token missing",
-		});
-	}
-
-	const token = authHeader.split(" ")[1];
-
+	const token = req.cookies.accessToken;
 	if (!token) {
 		return res.status(401).json({ message: "Token missing" });
 	}
 	try {
-		const decoded = jwt.verify(token, JWT_SECRET);
-
+		const decoded = verifyAccessToken(token);
 		req.user = decoded;
-
 		next();
 	} catch (error) {
 		return res.status(401).json({
--- apps/auth-service/src/utils/jwt.ts
+++ apps/auth-service/src/utils/jwt.ts
@@ -1,20 +1,30 @@
 import jwt from "jsonwebtoken";
-import dotenv from "dotenv";
-import { UUID } from "node:crypto";
-dotenv.config();
 
-const JWT_SECRET = process.env.AUTH_SECRET as string;
-const ACCESS_TOKEN_EXPIRY = "1h";
+const ACCESS_SECRET = process.env.AUTH_SECRET || "secret";
+const REFRESH_SECRET = process.env.AUTH_REFRESH_SECRET || "refresh";
+const ACCESS_TOKEN_EXPIRY = "15m";
+const REFRESH_TOKEN_EXPIRY = "7d";
 
-export const generateAccessToken = (payload: {
-	userId: string;
-	email: string;
-}) => {
-	return jwt.sign(payload, JWT_SECRET, {
+// generate access token
+export const generateAccessToken = (payload: { userId: string }) => {
+	return jwt.sign(payload, ACCESS_SECRET, {
 		expiresIn: ACCESS_TOKEN_EXPIRY,
 	});
 };
 
-export const verifyToken = (token: string) => {
-	return jwt.verify(token, JWT_SECRET);
+// generate refresh token
+export const generateRefreshToken = (payload: { userId: string }) => {
+	return jwt.sign(payload, REFRESH_SECRET, {
+		expiresIn: REFRESH_TOKEN_EXPIRY,
+	});
+};
+
+// verify access token
+export const verifyAccessToken = (token: string) => {
+	return jwt.verify(token, ACCESS_SECRET);
+};
+
+// verify refresh token
+export const verifyRefreshToken = (token: string) => {
+	return jwt.verify(token, REFRESH_SECRET);
 };
