f527d05b4dc45ba20594606e88258551e200de20
Ashminita <ashminitab@mindfiresolutions.com>
2026-02-18 18:09:24 +0530

    fix: fixed realations between roles ,permissions and users table

--- apps/auth-service/src/models/association.ts
+++ apps/auth-service/src/models/association.ts
@@ -2,7 +2,26 @@ import Role from "./role";
 import Permission from "./permission";
 import User from "./user";
 
-Role.hasMany(User, { foreignKey: "roleId" });
-User.belongsTo(Role, { foreignKey: "roleId" });
+User.belongsToMany(Role, {
+	through: "UserRoles",
+	foreignKey: "user_id",
+	otherKey: "role_id",
+});
+Role.belongsToMany(User, {
+	through: "UserRoles",
+	foreignKey: "role_id",
+	otherKey: "user_id",
+});
+
+Role.belongsToMany(Permission, {
+	through: "role_permissions",
+	foreignKey: "role_id",
+	otherKey: "permission_id",
+});
+Permission.belongsToMany(Role, {
+	through: "role_permissions",
+	foreignKey: "permission_id",
+	otherKey: "role_id",
+});
 
 export { Role, Permission, User };
