d6fe7e2db6ab5c1207c80c689da4f0bc26cbb3d6
Ashminita <ashminitab@mindfiresolutions.com>
2026-02-19 18:07:51 +0530

    feat: add routes for the paged

--- apps/frontend/src/routes/AppRoutes.tsx
+++ apps/frontend/src/routes/AppRoutes.tsx
@@ -1,6 +1,9 @@
+import AnalystDashboard from "@/pages/analyst/AnalystDashboard";
 import HomePage from "@/pages/common/HomePage";
 import Login from "@/pages/common/Login";
 import Register from "@/pages/common/Register";
+import ManagerDashboard from "@/pages/manager/ManagerDashboard";
+import OperationDashboard from "@/pages/operation-staffs/OperationDashboard";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function AppRoutes() {
@@ -18,6 +21,18 @@ function AppRoutes() {
 				<Route
 					path="/register"
 					element={<Register />}
+				/>
+				<Route
+					path="/analyst"
+					element={<AnalystDashboard />}
+				/>
+				<Route
+					path="/manager"
+					element={<ManagerDashboard />}
+				/>
+				<Route
+					path="/operations"
+					element={<OperationDashboard />}
 				/>
 			</Routes>
 		</BrowserRouter>
