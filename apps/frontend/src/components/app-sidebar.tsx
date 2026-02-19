47486a0e56b6e4ef8582adfba3c3ac365e98a243
Ashminita <ashminitab@mindfiresolutions.com>
2026-02-19 18:13:50 +0530

    feat: install shadcn components

--- apps/frontend/src/components/app-sidebar.tsx
+++ apps/frontend/src/components/app-sidebar.tsx
@@ -1,149 +1,102 @@
+"use client";
+
 import * as React from "react";
 import {
-	IconCamera,
-	IconChartBar,
-	IconDashboard,
-	IconDatabase,
-	IconFileAi,
-	IconFileDescription,
-	IconFileWord,
-	IconFolder,
-	IconHelp,
-	IconInnerShadowTop,
-	IconListDetails,
-	IconReport,
-	IconSearch,
-	IconSettings,
-	IconUsers,
-} from "@tabler/icons-react";
+	AudioWaveform,
+	BookOpen,
+	Command,
+	GalleryVerticalEnd,
+	Settings2,
+	SquareTerminal,
+} from "lucide-react";
 
-import { NavDocuments } from "@/components/nav-documents";
 import { NavMain } from "@/components/nav-main";
-import { NavSecondary } from "@/components/nav-secondary";
 import { NavUser } from "@/components/nav-user";
+import { TeamSwitcher } from "@/components/team-switcher";
 import {
 	Sidebar,
 	SidebarContent,
 	SidebarFooter,
 	SidebarHeader,
-	SidebarMenu,
-	SidebarMenuButton,
-	SidebarMenuItem,
+	SidebarRail,
 } from "@/components/ui/sidebar";
 
+// This is sample data.
 const data = {
 	user: {
 		name: "shadcn",
 		email: "m@example.com",
 		avatar: "/avatars/shadcn.jpg",
 	},
+	teams: [
+		{
+			name: "Manager",
+			logo: GalleryVerticalEnd,
+			plan: "Flight management",
+		},
+		{
+			name: "Airline Staff",
+			logo: AudioWaveform,
+			plan: "Operation Management",
+		},
+		{
+			name: "Analyst",
+			logo: Command,
+			plan: "Performance Management",
+		},
+	],
 	navMain: [
 		{
-			title: "Dashboard",
-			url: "#",
-			icon: IconDashboard,
-		},
-		{
-			title: "Lifecycle",
-			url: "#",
-			icon: IconListDetails,
-		},
-		{
-			title: "Analytics",
-			url: "#",
-			icon: IconChartBar,
-		},
-		{
-			title: "Projects",
-			url: "#",
-			icon: IconFolder,
-		},
-		{
-			title: "Team",
-			url: "#",
-			icon: IconUsers,
-		},
-	],
-	navClouds: [
-		{
-			title: "Capture",
-			icon: IconCamera,
+			title: "User Profile",
+			url: "/me",
+			icon: SquareTerminal,
 			isActive: true,
-			url: "#",
-			items: [
-				{
-					title: "Active Proposals",
-					url: "#",
-				},
-				{
-					title: "Archived",
-					url: "#",
-				},
-			],
 		},
 		{
-			title: "Proposal",
-			icon: IconFileDescription,
+			title: "Documentation",
 			url: "#",
+			icon: BookOpen,
 			items: [
 				{
-					title: "Active Proposals",
+					title: "Introduction",
 					url: "#",
 				},
 				{
-					title: "Archived",
+					title: "Get Started",
+					url: "#",
+				},
+				{
+					title: "Tutorials",
+					url: "#",
+				},
+				{
+					title: "Changelog",
 					url: "#",
 				},
 			],
 		},
-		{
-			title: "Prompts",
-			icon: IconFileAi,
-			url: "#",
-			items: [
-				{
-					title: "Active Proposals",
-					url: "#",
-				},
-				{
-					title: "Archived",
-					url: "#",
-				},
-			],
-		},
-	],
-	navSecondary: [
 		{
 			title: "Settings",
 			url: "#",
-			icon: IconSettings,
-		},
-		{
-			title: "Get Help",
-			url: "#",
-			icon: IconHelp,
-		},
-		{
-			title: "Search",
-			url: "#",
-			icon: IconSearch,
-		},
-	],
-	documents: [
-		{
-			name: "Data Library",
-			url: "#",
-			icon: IconDatabase,
-		},
-		{
-			name: "Reports",
-			url: "#",
-			icon: IconReport,
-		},
-		{
-			name: "Word Assistant",
-			url: "#",
-			icon: IconFileWord,
+			icon: Settings2,
+			items: [
+				{
+					title: "General",
+					url: "#",
+				},
+				{
+					title: "Team",
+					url: "#",
+				},
+				{
+					title: "Billing",
+					url: "#",
+				},
+				{
+					title: "Limits",
+					url: "#",
+				},
+			],
 		},
 	],
 };
@@ -151,35 +104,19 @@ const data = {
 export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
 	return (
 		<Sidebar
-			collapsible="offcanvas"
+			collapsible="icon"
 			{...props}
 		>
 			<SidebarHeader>
-				<SidebarMenu>
-					<SidebarMenuItem>
-						<SidebarMenuButton
-							asChild
-							className="data-[slot=sidebar-menu-button]:!p-1.5"
-						>
-							<a href="#">
-								<IconInnerShadowTop className="!size-5" />
-								<span className="text-base font-semibold">Acme Inc.</span>
-							</a>
-						</SidebarMenuButton>
-					</SidebarMenuItem>
-				</SidebarMenu>
+				<TeamSwitcher teams={data.teams} />
 			</SidebarHeader>
 			<SidebarContent>
 				<NavMain items={data.navMain} />
-				<NavDocuments items={data.documents} />
-				<NavSecondary
-					items={data.navSecondary}
-					className="mt-auto"
-				/>
 			</SidebarContent>
 			<SidebarFooter>
 				<NavUser user={data.user} />
 			</SidebarFooter>
+			<SidebarRail />
 		</Sidebar>
 	);
 }
--- apps/frontend/src/components/signup-form.tsx
+++ apps/frontend/src/components/signup-form.tsx
@@ -13,8 +13,43 @@ import {
 	FieldLabel,
 } from "@/components/ui/field";
 import { Input } from "@/components/ui/input";
+import { useState } from "react";
+import axios from "axios";
+
+// interface SignupFormProps{
+// 	onSubmit:(data:{
+// 		name:string,
+// 		email:string,
+// 		phone:string,
+// 		password:string;
+// 	})=>void
+// }
 
 export function SignupForm({ ...props }: React.ComponentProps<typeof Card>) {
+	const [formData, setFormData] = useState({
+		username: "",
+		email: "",
+		phone: "",
+		password: "",
+	});
+	const [message, setMessage] = useState("");
+
+	const handleChange = (e: { target: { name: any; value: any } }) => {
+		setFormData({ ...formData, [e.target.name]: e.target.value });
+	};
+
+	const handleSubmit = async (e: { preventDefault: () => void }) => {
+		e.preventDefault();
+		try {
+			const res = await axios.post(
+				"http://localhost:3000/api/auth/register",
+				formData,
+			);
+			setMessage(res.data.message);
+		} catch (err) {
+			setMessage("Something went wrong");
+		}
+	};
 	return (
 		<Card {...props}>
 			<CardHeader>
@@ -24,11 +59,12 @@ export function SignupForm({ ...props }: React.ComponentProps<typeof Card>) {
 				</CardDescription>
 			</CardHeader>
 			<CardContent>
-				<form>
+				<form onSubmit={handleSubmit}>
 					<FieldGroup>
 						<Field>
 							<FieldLabel htmlFor="name">Full Name</FieldLabel>
 							<Input
+								onChange={handleChange}
 								id="name"
 								type="text"
 								placeholder="John Doe"
@@ -38,6 +74,7 @@ export function SignupForm({ ...props }: React.ComponentProps<typeof Card>) {
 						<Field>
 							<FieldLabel htmlFor="email">Email</FieldLabel>
 							<Input
+								onChange={handleChange}
 								id="email"
 								type="email"
 								placeholder="m@example.com"
@@ -49,8 +86,22 @@ export function SignupForm({ ...props }: React.ComponentProps<typeof Card>) {
 							</FieldDescription>
 						</Field>
 						<Field>
+							<FieldLabel htmlFor="phone">Phone</FieldLabel>
+							<Input
+								onChange={handleChange}
+								id="phone"
+								type="number"
+								placeholder="+91-96785432"
+								required
+							/>
+							<FieldDescription>
+								We&apos;ll use this to send you message.
+							</FieldDescription>
+						</Field>
+						<Field>
 							<FieldLabel htmlFor="password">Password</FieldLabel>
 							<Input
+								onChange={handleChange}
 								id="password"
 								type="password"
 								required
@@ -58,17 +109,6 @@ export function SignupForm({ ...props }: React.ComponentProps<typeof Card>) {
 							<FieldDescription>
 								Must be at least 8 characters long.
 							</FieldDescription>
-						</Field>
-						<Field>
-							<FieldLabel htmlFor="confirm-password">
-								Confirm Password
-							</FieldLabel>
-							<Input
-								id="confirm-password"
-								type="password"
-								required
-							/>
-							<FieldDescription>Please confirm your password.</FieldDescription>
 						</Field>
 						<FieldGroup>
 							<Field>
@@ -80,12 +120,13 @@ export function SignupForm({ ...props }: React.ComponentProps<typeof Card>) {
 									Sign up with Google
 								</Button>
 								<FieldDescription className="px-6 text-center">
-									Already have an account? <a href="#">Sign in</a>
+									Already have an account? <a href="/login">Sign in</a>
 								</FieldDescription>
 							</Field>
 						</FieldGroup>
 					</FieldGroup>
 				</form>
+				{message && <p>{message}</p>}
 			</CardContent>
 		</Card>
 	);
