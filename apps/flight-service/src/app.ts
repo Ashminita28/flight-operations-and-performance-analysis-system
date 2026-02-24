9f9bd6669a929b36b7fd7de1115969adcb70aaeb
Ashminita <ashminitab@mindfiresolutions.com>
2026-02-24 13:23:53 +0530

    fix: connection issue

--- apps/flight-service/src/app.ts
+++ apps/flight-service/src/app.ts
@@ -1,4 +1,5 @@
 import express, { Express } from "express";
+import "../src/models/index";
 
 const app: Express = express();
 app.use(express.json());
--- apps/flight-service/src/services/flight-service.ts
+++ apps/flight-service/src/services/flight-service.ts
@@ -1,70 +1,66 @@
-import "@repo/shared-databse";
+// import Crew from "../models/crew";
+// import Flight from "../models/flight";
 
-// create flight
-export const createFlightService = async (
-	aircraft_id: string,
-	route_id: string,
-	departure_time: string,
-) => {
-	const aircraft = await Aircraft.findByPk(aircraft_id);
-	if (!aircraft) {
-		throw new Error("Aircraft not found");
-	}
-	if (aircraft.maintenance_status === "Under Maintenance") {
-		throw new Error("Aircraft is under maintenance");
-	}
-	const route = await Route.findByPk(route_id);
-	if (!route) {
-		throw new Error("Route not found");
-	}
-	const existingFlight = await Flight.findOne({
-		where: { aircraft_id, departure_time },
-	});
-	if (existingFlight) {
-		throw new Error("Aircraft already assigned to another flight at this time");
-	}
-	return await Flight.create(data);
-};
+// export const getAircraft=async()=>{
 
-// update flight
-export const updateFlighService = async (id: string, data: any) => {
-	const flight = await Flight.findByPk(id);
-	if (!flight) {
-		throw new Error("Flight not found");
-	}
-	await flight.update(data);
-	return flight;
-};
+// }
 
-// get all flights
-export const getAllFlightsService = async () => {
-	return await Flight.findAll({
-		include: [{ model: Aircraft }, { model: Route }],
-	});
-};
+// // create flight
+// export const createFlightService = async (
+// 	aircraft_id: string,
+// 	route_id: string,
+// 	departure_time: string,
+// ) => {
+// 	const aircraft = await Aircraft.findByPk(aircraft_id);
+// 	if (!aircraft) {
+// 		throw new Error("Aircraft not found");
+// 	}
+// 	if (aircraft.maintenance_status === "Under Maintenance") {
+// 		throw new Error("Aircraft is under maintenance");
+// 	}
+// 	const crew=await Crew.findByPk(crew_id);
+// 	if(!crew){
+// 		throw new Error("Crew not available");
+// 	}
 
-// get flight by ID
-export const getFlightByIdService = async (id: string) => {
-	const flight = await Flight.findByPk(id, { include: [Aircraft, Route] });
-};
+// 	const existingFlight = await Flight.findOne({
+// 		where: { aircraft_id, departure_time },
+// 	});
+// 	if (existingFlight) {
+// 		throw new Error("Aircraft already assigned to another flight at this time");
+// 	}
 
-// update flight status
-export const updateFlightStatus = async (id: string, status: string) => {
-	const flight = await Flight.findByPk(id);
-	if (!flight) {
-		throw new Error("Flight not found");
-		await flight.update({ status });
-		return flight;
-	}
-};
+// 	return await Flight.create(data);
+// };
 
-// delete flight service
-export const deleteFlightService = async (id: string) => {
-	const flight = await Flight.findByPk(id);
-	if (!flight) {
-		throw new Error("Flight not found");
-	}
+// // update flight
+// export const updateFlighService = async (id: string, data: any) => {
+// 	const flight = await Flight.findByPk(id);
+// 	if (!flight) {
+// 		throw new Error("Flight not found");
+// 	}
+// 	await flight.update(data);
+// 	return flight;
+// };
 
-	await flight.destroy();
-	return true;
-};
+// // get all flights
+// export const getAllFlightsService = async () => {
+// 	return await Flight.findAll({
+// 		include: [{ model: Aircraft }, { model: Route }],
+// 	});
+// };
+
+// // get flight by ID
+// export const getFlightByIdService = async (id: string) => {
+// 	const flight = await Flight.findByPk(id, { include: [Aircraft] });
+// };
+
+// // update flight status
+// export const updateFlightStatus = async (id: string, status: string) => {
+// 	const flight = await Flight.findByPk(id);
+// 	if (!flight) {
+// 		throw new Error("Flight not found");
+// 		// await flight.update({ status });
+// 		// return flight;
+// 	}
+// };
