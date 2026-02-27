d609f20769014917f240e6fe6619103d06863461
Ashminita <ashminitab@mindfiresolutions.com>
2026-02-27 18:14:45 +0530

    feat(frontend): add protected routes,with role access to pages

--- apps/frontend/src/routes/AppRoutes.tsx
+++ apps/frontend/src/routes/AppRoutes.tsx
@@ -1,16 +1,22 @@
-import AnalystDashboard from "@/pages/analyst/AnalystDashboard";
 import HomePage from "@/pages/common/HomePage";
 import Login from "@/pages/common/Login";
-import Register from "@/pages/common/Register";
-import ManagerDashboard from "@/pages/manager/ManagerDashboard";
-import OperationDashboard from "@/pages/operation-staffs/OperationDashboard";
 import AdminDashboard from "@/pages/admin/AdminDashboard";
 import Profile from "@/pages/common/Profile";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import ProtectedRoute from "./ProtectedRoute";
 import ProfileDashboard from "@/pages/common/ProfileDashboard";
 import ForgotPassword from "@/pages/common/ForgotPassword";
 import ResetPassword from "@/pages/common/ResetPassword";
+import OperationDashboard from "@/pages/operations/OperationDashboard";
+import { AircraftRegistration } from "@/pages/operations/AircraftRegistration";
+import { Crew } from "@/pages/operations/Crew";
+import { FlightEvent } from "@/pages/operations/FlightEvent";
+import { FlightRegistration } from "@/pages/operations/FlightRegistration";
+import { Maintainanace } from "@/pages/operations/Maintainance";
+
+import UserMangement from "@/pages/admin/UserMangement";
+import FlightDashboard from "@/pages/FlightDashboard";
+
+import ProtectedRoute from "./ProtectedRoute";
 
 function AppRoutes() {
 	return (
@@ -20,35 +26,98 @@ function AppRoutes() {
 					path="/"
 					element={<HomePage />}
 				/>
+
 				<Route
 					path="/login"
-					element={
-						<ProtectedRoute>
-							<Login />
-						</ProtectedRoute>
-					}
+					element={<Login />}
 				/>
-				<Route
-					path="/register"
-					element={<Register />}
-				/>
+
 				<Route
 					path="/forget-password"
 					element={<ForgotPassword />}
 				/>
+
 				<Route
 					path="/reset-password"
 					element={<ResetPassword />}
 				/>
-				<Route
-					path="/main-dashboard"
-					element={<ProfileDashboard />}
-				/>
+
+				{/* ADMIN ROUTES */}
+
 				<Route
 					path="/admin"
 					element={
-						<ProtectedRoute>
+						<ProtectedRoute allowedRoles={["Admin"]}>
 							<AdminDashboard />
+						</ProtectedRoute>
+					}
+				/>
+
+				<Route
+					path="/register"
+					element={
+						<ProtectedRoute allowedRoles={["Admin"]}>
+							<UserMangement />
+						</ProtectedRoute>
+					}
+				/>
+
+				<Route
+					path="/operations"
+					element={
+						<ProtectedRoute allowedRoles={["Operations"]}>
+							<OperationDashboard />
+						</ProtectedRoute>
+					}
+				/>
+
+				<Route
+					path="/aircraft"
+					element={
+						<ProtectedRoute allowedRoles={["Operations"]}>
+							<AircraftRegistration />
+						</ProtectedRoute>
+					}
+				/>
+
+				<Route
+					path="/crew"
+					element={
+						<ProtectedRoute allowedRoles={["Operations"]}>
+							<Crew />
+						</ProtectedRoute>
+					}
+				/>
+				<Route
+					path="/flightevent"
+					element={
+						<ProtectedRoute allowedRoles={["Operations"]}>
+							<FlightEvent />
+						</ProtectedRoute>
+					}
+				/>
+				<Route
+					path="/flightregistration"
+					element={
+						<ProtectedRoute allowedRoles={["Operations"]}>
+							<FlightRegistration />
+						</ProtectedRoute>
+					}
+				/>
+
+				<Route
+					path="/maintainance"
+					element={
+						<ProtectedRoute allowedRoles={["Operations"]}>
+							<Maintainanace />
+						</ProtectedRoute>
+					}
+				/>
+				<Route
+					path="/flight-dashboard"
+					element={
+						<ProtectedRoute allowedRoles={["Manager", "Admin"]}>
+							<FlightDashboard />
 						</ProtectedRoute>
 					}
 				/>
@@ -60,27 +129,12 @@ function AppRoutes() {
 						</ProtectedRoute>
 					}
 				/>
+
 				<Route
-					path="/analyst"
+					path="/main-dashboard"
 					element={
-						<ProtectedRoute allowedRoles={["Analyst"]}>
-							<AnalystDashboard />
-						</ProtectedRoute>
-					}
-				/>
-				<Route
-					path="/manager"
-					element={
-						<ProtectedRoute allowedRoles={["Manager"]}>
-							<ManagerDashboard />
-						</ProtectedRoute>
-					}
-				/>
-				<Route
-					path="/operations"
-					element={
-						<ProtectedRoute allowedRoles={["Operations"]}>
-							<OperationDashboard />
+						<ProtectedRoute>
+							<ProfileDashboard />
 						</ProtectedRoute>
 					}
 				/>
