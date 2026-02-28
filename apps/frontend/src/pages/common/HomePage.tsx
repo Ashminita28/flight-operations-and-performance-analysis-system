6a358c639dd18266181a95cd7436c70bca586986
Ashminita <ashminitab@mindfiresolutions.com>
2026-02-28 23:26:41 +0530

    refactor(ui): change homepage design

--- apps/frontend/src/pages/common/HomePage.tsx
+++ apps/frontend/src/pages/common/HomePage.tsx
@@ -1,27 +1,33 @@
 import { Navbar1 } from "@/components/navbar1";
-import { useAuthStore } from "../../store/auth-store";
+import flight from "../../assets/flight.jpeg";
+import { Button } from "@/components/ui/button";
 
 export default function HomePage() {
-	const user = useAuthStore(s => s.user);
-
 	return (
-		<div className="min-h-screen bg-sky-950 text-white">
-			<Navbar1 />
-
-			<div className="flex flex-col items-center justify-center h-[80vh] text-center px-6">
-				<h1 className="text-5xl font-bold mb-6">
-					Flight Operations Management System
-				</h1>
-
-				<p className="max-w-2xl text-lg opacity-80 mb-6">
-					Manage flights, aircraft, crew and operations efficiently using Fligo
-					Aviation Platform.
-				</p>
-
-				{user && (
-					<p className="text-sm opacity-70">Logged in as {user.email}</p>
-				)}
+		<div
+			className="absolute inset-0 bg-cover bg-center"
+			style={{ backgroundImage: `url(${flight})` }}
+		>
+			<div className="absolute inset-0 bg-black/50">
+				<Navbar1 />
+				<div className="relative z-10 flex h-full items-center">
+					<div className="max-w-2xl pl-24">
+						<p className="text-200 font-bold mb-3 text-lg">
+							Flight Operations Management System
+						</p>
+						<h1 className="text-5xl font-bold leading-tight mb-6">
+							Centralized Flight Operations & Performance <br /> Analytics
+						</h1>
+						<p className="text-300 font-bold text-lg mb-8">
+							Monitor flights in real-time, track delays, and analyze flight
+							performance - all in one system.
+						</p>
+						<Button size="lg">Explore</Button>
+					</div>
+				</div>
 			</div>
+
+			<div></div>
 		</div>
 	);
 }
