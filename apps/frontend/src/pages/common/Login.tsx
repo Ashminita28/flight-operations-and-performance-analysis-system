7abe26e49ac59688ca15160b89c0e8c9161416d4
Ashminita <ashminitab@mindfiresolutions.com>
2026-03-02 02:15:36 +0530

    refactor(frontend): add background image

--- apps/frontend/src/pages/common/Login.tsx
+++ apps/frontend/src/pages/common/Login.tsx
@@ -1,22 +1,27 @@
-import { GalleryVerticalEnd } from "lucide-react";
-
 import { LoginForm } from "@/components/login-form";
+import flight from "../../assets/flight.jpeg";
 
 export default function Login() {
 	return (
-		<div className="bg-muted flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
-			<div className="flex w-full max-w-sm flex-col gap-6">
-				<a
-					href="/"
-					className="flex items-center gap-2 self-center font-medium"
-				>
-					<div className="bg-primary text-primary-foreground flex size-6 items-center justify-center rounded-md">
-						<GalleryVerticalEnd className="size-4" />
+		<div
+			className="absolute inset-0 bg-cover bg-center"
+			style={{ backgroundImage: `url(${flight})` }}
+		>
+			<div className="absolute inset-0 bg-black/50">
+				<div className=" flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
+					<div className="flex w-full max-w-sm flex-col gap-6">
+						<a
+							href="/"
+							className="flex items-center gap-2 self-center font-medium"
+						>
+							Fligo
+						</a>
+						<LoginForm />
 					</div>
-					Fligo
-				</a>
-				<LoginForm />
+				</div>
 			</div>
+
+			<div></div>
 		</div>
 	);
 }
