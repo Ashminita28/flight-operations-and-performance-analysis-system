60cc79a5815758075c3c78ca46a81396b71db81f
Ashminita <ashminitab@mindfiresolutions.com>
2026-03-01 16:26:56 +0530

    refactor(aircraft): change role access of routes

--- apps/aircraft-service/src/routes/aircraft-route.ts
+++ apps/aircraft-service/src/routes/aircraft-route.ts
@@ -1,54 +1,59 @@
 import { Router } from "express";
-import {
-	createAircraft,
-	getAircraft,
-	getAircraftById,
-	updateAircraft,
-	deleteAircraft,
-	addMaintenance,
-} from "../controllers/aircraft-controller";
 
-import constants from "../utils/constants";
 import { authenticate } from "@package/shared-middleware";
 import { authorizeRole } from "@package/shared-middleware";
+import {
+	changeAircraftStatusController,
+	createAircraftController,
+	deleteAircraftByIdController,
+	getAircraftByIdController,
+	getAllAircraftController,
+	getAllAirportsController,
+	updateAircraftByIdController,
+} from "../controllers/aircraft-controller";
 
 const aircraftRouter: Router = Router();
 
 aircraftRouter.post(
-	"/",
+	"/aircraft",
 	authenticate,
 	authorizeRole(["Operations"]),
-	createAircraft,
+	createAircraftController,
 );
 aircraftRouter.get(
-	"/",
+	"/aircraft",
 	authenticate,
-	authorizeRole(["Operations", "Manager"]),
-	getAircraft,
+	authorizeRole(["Manager", "Operations", "Analyst", "Admin"]),
+	getAllAircraftController,
 );
 aircraftRouter.get(
-	"/:id",
+	"/aircraft/:id",
 	authenticate,
-	authorizeRole(["Operations", "Manager"]),
-	getAircraftById,
+	authorizeRole(["Manager", "Operations", "Analyst", "Admin"]),
+	getAircraftByIdController,
 );
 aircraftRouter.put(
-	"/:id",
-	authenticate,
-	authorizeRole(["Operations", "Manager"]),
-	updateAircraft,
-);
-aircraftRouter.delete(
-	"/:id",
-	authenticate,
-	authorizeRole(["Manager"]),
-	deleteAircraft,
-);
-aircraftRouter.post(
-	"/:id/maintenance",
+	"/aircraft/:id",
 	authenticate,
 	authorizeRole(["Operations"]),
-	addMaintenance,
+	updateAircraftByIdController,
+);
+aircraftRouter.patch(
+	"/aircraft/:id/status",
+	authorizeRole(["Operations"]),
+	changeAircraftStatusController,
+);
+aircraftRouter.delete(
+	"/aircraft/:id",
+	authenticate,
+	authorizeRole(["Operations"]),
+	deleteAircraftByIdController,
+);
+aircraftRouter.get(
+	"/airports",
+	authenticate,
+	authorizeRole(["Manager", "Operations", "Analyst", "Admin"]),
+	getAllAirportsController,
 );
 
 export default aircraftRouter;
