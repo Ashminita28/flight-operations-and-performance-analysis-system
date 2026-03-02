a14acd4e59b1e657fe61bb788bbeb534e5c69602
Ashminita <ashminitab@mindfiresolutions.com>
2026-03-02 18:46:02 +0530

    feat(ops-event): add routes for record operation and update

--- apps/operational-event-service/src/routes/operation-route.ts
+++ apps/operational-event-service/src/routes/operation-route.ts
@@ -3,28 +3,13 @@ import { OperationController } from "../controllers/operation-controller";
 
 const operationRouter: Router = Router();
 
-operationRouter.get(
-	"/flights/:flightId/events",
-	OperationController.allFlightEvents,
-);
 operationRouter.post(
 	"/flights/:flightId/events",
 	OperationController.recordFlightEvent,
-);
-operationRouter.get(
-	"/flights/:flightId/events/:id",
-	OperationController.specificFlightEvent,
 );
 operationRouter.put(
 	"/flights/:flightId/events/:id",
 	OperationController.changeFlightEvent,
 );
-operationRouter.patch(
-	"/flights/:flightId/events/:id/resolve",
-	OperationController.resolveFlightEvent,
-);
-operationRouter.get("/events", OperationController.searchFlightEvent);
-operationRouter.get("/delay-categories", OperationController.delayCategories);
-operationRouter.post("/delay-categories", OperationController.newDelayCategory);
 
 export default operationRouter;
