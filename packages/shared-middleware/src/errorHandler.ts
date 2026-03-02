9f4a45fba0994a939f6f544cac9257ba2e8d1b38
Ashminita <ashminitab@mindfiresolutions.com>
2026-03-02 10:43:04 +0530

    fix(middleware): fix error handler middleware

--- packages/shared-middleware/src/errorHandler.ts
+++ packages/shared-middleware/src/errorHandler.ts
@@ -1,10 +1,10 @@
 import { Request, Response, NextFunction } from "express";
 
 export const errorHandler = (
+	err: any,
 	req: Request,
 	res: Response,
 	next: NextFunction,
-	err: any,
 ) => {
 	console.error(err);
 
