8872d9b5cd531d7ab4f8403d1d9238c902af91f1
Ashminita <ashminitab@mindfiresolutions.com>
2026-03-03 12:25:01 +0530

    refactor(ops-event): add find delay by code , by id,delete delay repository

--- apps/operational-event-service/src/repositories/delaycategory-repository.ts
+++ apps/operational-event-service/src/repositories/delaycategory-repository.ts
@@ -1,6 +1,23 @@
-import { OperationalEvent } from "@package/shared-database";
+import { DelayCategory } from "@package/shared-database";
 
-export const findById = async (id: string) => {
-	const delay = await OperationalEvent.findByPk(id);
-	return delay;
+export const DelayCategoryRepository = {
+	async create(data: { code: string; description: string }) {
+		return DelayCategory.create(data as any);
+	},
+
+	async findByCode(code: string) {
+		return DelayCategory.findOne({ where: { code } });
+	},
+
+	async findAll() {
+		return DelayCategory.findAll();
+	},
+
+	async findById(id: string) {
+		return DelayCategory.findByPk(id);
+	},
+
+	async delete(id: string) {
+		return DelayCategory.destroy({ where: { id } });
+	},
 };
