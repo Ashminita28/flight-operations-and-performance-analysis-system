d78e1ae21d1fecd26cd2a5715a6815d301a5382d
Ashminita <ashminitab@mindfiresolutions.com>
2026-03-03 12:52:27 +0530

    refactor(ops-event): add delay router,add express middleware

--- apps/operational-event-service/src/app.ts
+++ apps/operational-event-service/src/app.ts
@@ -2,11 +2,14 @@ import express, { Express } from "express";
 import helmet from "helmet";
 import morgan from "morgan";
 import operationRouter from "./routes/operation-route";
+import delayRouter from "./routes/delay-routes";
 
 const app: Express = express();
 
-app.use(helmet);
+app.use(helmet());
 app.use(morgan("dev"));
+app.use(express.json());
 app.use("/api", operationRouter);
+app.use("/api", delayRouter);
 
 export default app;
