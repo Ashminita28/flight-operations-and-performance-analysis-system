87b59006ceb9f6723b2c746894fc32e7d79bfd8f
Ashminita <ashminitab@mindfiresolutions.com>
2026-03-05 00:05:44 +0530

    feat(frontend): add store for create , fetch events in operational service

---
+++ apps/frontend/src/store/operation-state.ts
@@ -0,0 +1,46 @@
+import { create } from "zustand";
+import { api } from "../api/api";
+import type { OperationalEvent } from "@/types/flight-types";
+
+interface OperationStore {
+	events: OperationalEvent[];
+	loading: boolean;
+	error: string | null;
+
+	fetchEvents: (flight_id: string) => Promise<void>;
+	addEvent: (flightId: string, eventData: any) => Promise<void>;
+}
+
+export const useOperationStore = create<OperationStore>(set => ({
+	events: [],
+	loading: false,
+	error: null,
+
+	fetchEvents: async (flight_id: string) => {
+		set({ loading: true, error: null });
+
+		try {
+			const res = await api<{ success: boolean; data: OperationalEvent[] }>(
+				`/operations/${flight_id}/events`,
+			);
+			set({ events: res.data, loading: false });
+		} catch (err: any) {
+			set({ error: err.message, loading: false });
+		}
+	},
+
+	addEvent: async (flightId, eventData) => {
+		set({ loading: true, error: null });
+
+		try {
+			await api(`/operations/${flightId}/events`, {
+				method: "POST",
+				body: JSON.stringify(eventData),
+			});
+
+			await useOperationStore.getState().fetchEvents(flightId);
+		} catch (err: any) {
+			set({ error: err.message, loading: false });
+		}
+	},
+}));
