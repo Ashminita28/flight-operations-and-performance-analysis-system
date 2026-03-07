f2a8b2c2ed2268fcf6ab8ce0ee9082614d4079cd
Ashminita <ashminitab@mindfiresolutions.com>
2026-03-08 00:45:33 +0530

    refactor(middleware): export function specific

--- packages/shared-middleware/src/index.ts
+++ packages/shared-middleware/src/index.ts
@@ -1,3 +1,3 @@
-export * from "./auth";
-export * from "./errorHandler";
-export * from "./role";
+export { authenticate } from "./auth";
+export { errorHandler } from "./errorHandler";
+export { authorizeRole } from "./role";
