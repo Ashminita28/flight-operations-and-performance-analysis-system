f849edf10efa6af029b1280e7665efd973928c3a
Ashminita <ashminitab@mindfiresolutions.com>
2026-03-08 14:23:32 +0530

    feat(ops-event): add env example, move port to env

--- apps/operational-event-service/src/app.ts
+++ apps/operational-event-service/src/app.ts
@@ -1,6 +1,8 @@
 import express, { Express } from "express";
 import helmet from "helmet";
 import morgan from "morgan";
+import cookieParser from "cookie-parser";
+import cors from "cors";
 import operationRouter from "./routes/operation-route";
 import delayRouter from "./routes/delay-routes";
 
@@ -9,6 +11,13 @@ const app: Express = express();
 app.use(helmet());
 app.use(morgan("dev"));
 app.use(express.json());
+app.use(cookieParser());
+app.use(
+	cors({
+		origin: "http://localhost:5173",
+		credentials: true,
+	}),
+);
 app.use("/api/operations", operationRouter);
 app.use("/api", delayRouter);
 
--- apps/operational-event-service/src/server.ts
+++ apps/operational-event-service/src/server.ts
@@ -3,9 +3,10 @@ dotenv.config();
 import app from "./app";
 import { initializeModels } from "@package/shared-database";
 
+const PORT = process.env.PORT;
 const start = async () => {
 	await initializeModels();
-	app.listen(3004, () => {
+	app.listen(PORT, () => {
 		console.log("Operational event running at port 3004");
 	});
 };
