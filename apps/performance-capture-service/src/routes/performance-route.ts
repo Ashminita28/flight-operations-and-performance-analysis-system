ca7ee6d4779fe1cfda4cc5f74d6e39ae96c76ab5
Ashminita <ashminitab@mindfiresolutions.com>
2026-03-08 14:27:57 +0530

    refactor(performance): add role access to the routes

--- apps/performance-capture-service/src/routes/performance-route.ts
+++ apps/performance-capture-service/src/routes/performance-route.ts
@@ -1,10 +1,28 @@
 import { Router } from "express";
 import { FlightPerformanceController } from "../controller/performance-controller";
 
+import { authenticate } from "@package/shared-middleware";
+import { authorizeRole } from "@package/shared-middleware";
 const performanceRouter: Router = Router();
 
-performanceRouter.post("/performance", FlightPerformanceController.create);
+performanceRouter.post(
+	"/performance/:flightId",
+	authenticate,
+	authorizeRole("Operations"),
+	FlightPerformanceController.create,
+);
 
-performanceRouter.get("/performance", FlightPerformanceController.getAll);
+performanceRouter.get(
+	"/performance",
+	authenticate,
+	authorizeRole("Manager", "Operations", "Analyst", "Admin"),
+	FlightPerformanceController.getAll,
+);
+performanceRouter.get(
+	"/performance/:flight_id",
+	authenticate,
+	authorizeRole("Manager", "Operations", "Analyst", "Admin"),
+	FlightPerformanceController.geFlightPerformance,
+);
 
 export default performanceRouter;
