f0fc9bddd47ddf0adfc24cb9765fe4d4b9b41e89
Ashminita <ashminitab@mindfiresolutions.com>
2026-03-10 18:17:12 +0530

    feat(analytics): add controller for delay analytics

--- apps/worker-service/src/controller/analytics-controller.ts
+++ apps/worker-service/src/controller/analytics-controller.ts
@@ -4,7 +4,10 @@ import { analyticsService } from "../services/analytics-service";
 export const analyticsController = {
 	async getCounterController(req: Request, res: Response, next: NextFunction) {
 		try {
-			const counter = await analyticsService.getDashboardCounterService();
+			const counter = await analyticsService.getDashboardCounterService(
+				req.query,
+			);
+
 			res.status(200).json({
 				success: true,
 				message: "Counter are here",
@@ -14,13 +17,31 @@ export const analyticsController = {
 			next(error);
 		}
 	},
+
+	// ON TIME PERFORMANCE CHART
 	async getOnTimeController(req: Request, res: Response, next: NextFunction) {
 		try {
-			const onTime = await analyticsService.getOnTimePerformanceChart();
+			const onTime = await analyticsService.getOnTimePerformance(req.query);
+
 			res.status(200).json({
 				success: true,
 				message: "on time performance",
 				data: onTime,
+			});
+		} catch (error) {
+			next(error);
+		}
+	},
+
+	// DELAY ANALYTICS PIE CHART
+	async getDelayAnalytics(req: Request, res: Response, next: NextFunction) {
+		try {
+			const result = await analyticsService.getDelayAnalytics(req.query);
+
+			res.status(200).json({
+				success: true,
+				message: "delay analytics",
+				data: result,
 			});
 		} catch (error) {
 			next(error);
