992483f9afbcc521b986bf481fc6266b14135394
Ashminita <ashminitab@mindfiresolutions.com>
2026-03-10 14:54:25 +0530

    feat(frontend): allow admin to access the register user page

--- apps/frontend/src/components/nav-main.tsx
+++ apps/frontend/src/components/nav-main.tsx
@@ -9,6 +9,7 @@ import {
 	SidebarMenuItem,
 } from "@/components/ui/sidebar";
 import { useNavigate } from "react-router-dom";
+import { useAuthStore } from "@/store/auth-store";
 
 export function NavMain({
 	items,
@@ -20,30 +21,32 @@ export function NavMain({
 	}[];
 }) {
 	const navigate = useNavigate();
+	const user = useAuthStore(s => s.user);
 	return (
 		<SidebarGroup>
 			<SidebarGroupContent className="flex flex-col gap-2">
 				<SidebarMenu>
-					<SidebarMenuItem className="flex items-center gap-2">
-						<SidebarMenuButton
-							onClick={() => {
-								navigate("/register");
-							}}
-							tooltip="Quick Create"
-							className="bg-primary text-primary-foreground hover:bg-primary/90 hover:text-primary-foreground active:bg-primary/90 active:text-primary-foreground min-w-8 duration-200 ease-linear"
-						>
-							<IconCirclePlusFilled />
-							<span>Register User</span>
-						</SidebarMenuButton>
-						<Button
-							size="icon"
-							className="size-8 group-data-[collapsible=icon]:opacity-0"
-							variant="outline"
-						>
-							<IconMail />
-							<span className="sr-only">Inbox</span>
-						</Button>
-					</SidebarMenuItem>
+					{user?.roles?.includes("Admin") && (
+						<SidebarMenuItem className="flex items-center gap-2">
+							<SidebarMenuButton
+								onClick={() => navigate("/register")}
+								tooltip="Quick Create"
+								className="bg-primary text-primary-foreground hover:bg-primary/90 hover:text-primary-foreground active:bg-primary/90 active:text-primary-foreground min-w-8 duration-200 ease-linear"
+							>
+								<IconCirclePlusFilled />
+								<span>Register User</span>
+							</SidebarMenuButton>
+
+							<Button
+								size="icon"
+								className="size-8 group-data-[collapsible=icon]:opacity-0"
+								variant="outline"
+							>
+								<IconMail />
+								<span className="sr-only">Inbox</span>
+							</Button>
+						</SidebarMenuItem>
+					)}
 				</SidebarMenu>
 				<SidebarMenu>
 					{items.map(item => (
