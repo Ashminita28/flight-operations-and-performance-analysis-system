5127e2335583cfa594459ff0a563a39475705ec3
Ashminita <ashminitab@mindfiresolutions.com>
2026-03-13 15:20:14 +0530

    refactor: add swagger ui

--- apps/auth-service/src/app.ts
+++ apps/auth-service/src/app.ts
@@ -7,6 +7,55 @@ import userRouter from "./routes/user-routes";
 import morgan from "morgan";
 import cors from "cors";
 
+const swaggerJSDoc = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
+import path from "path";
+
+// SWAGGER API
+const swaggerDefinition = {
+	openapi: "3.0.0",
+	info: {
+		title: "LMS API",
+		version: "1.0.0",
+		description: "This is a REST API application made with Express.",
+		license: {
+			name: "Licensed Under MIT",
+			url: "https://spdx.org/licenses/MIT.html",
+		},
+		contact: {
+			name: "JSONPlaceholder",
+			url: "https://jsonplaceholder.typicode.com",
+		},
+	},
+	components: {
+		securitySchemes: {
+			bearerAuth: {
+				type: "http",
+				scheme: "bearer",
+				bearerFormat: "JWT",
+			},
+		},
+	},
+	security: [
+		{
+			beareAuth: [],
+		},
+	],
+	servers: [
+		{
+			url: "http://localhost:3000",
+			description: "Development server",
+		},
+	],
+};
+
+const options = {
+	swaggerDefinition,
+	// Paths to files containing OpenAPI definitions
+	apis: [path.join(__dirname, "routes", "*.ts")],
+};
+const swaggerSpec = swaggerJSDoc(options);
+
 const app: Express = express();
 app.use(
 	cors({
@@ -24,4 +73,6 @@ app.use("/api", passwordRouter);
 app.use("/api", adminRouter);
 app.use("/api", userRouter);
 
+// DISPLAY THE SWAGGER UI
+app.use("/api/docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 export default app;
