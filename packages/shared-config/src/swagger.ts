dcf9e84e8f25f61e3b50eee7470c7c27c8e7c98f
Ashminita <ashminitab@mindfiresolutions.com>
2026-03-20 18:44:24 +0530

    fix(config): swagger docs issue

--- packages/shared-config/src/swagger.ts
+++ packages/shared-config/src/swagger.ts
@@ -1,19 +1,24 @@
 import swaggerJsdoc from "swagger-jsdoc";
-
-export const createSwaggerSpec = (serviceName: string, port: number) => {
-	return swaggerJsdoc({
-		definition: {
-			openapi: "3.0.0",
-			info: {
-				title: `${serviceName} API`,
-				version: "1.0.0",
-			},
-			servers: [
-				{
-					url: `http://localhost:${port}`,
-				},
-			],
-		},
-		apis: ["dist/routes/**/*.js"],
-	});
+import path from "path";
+ 
+export const createSwaggerSpec = (serviceName: string, basePath: string) => {
+  return swaggerJsdoc({
+    definition: {
+      openapi: "3.0.0",
+      info: {
+        title: `${serviceName} API`,
+        version: "1.0.0",
+      },
+      servers: [
+        {
+          url: basePath,
+        },
+      ],
+    },
+ 
+    apis: [
+      path.join(process.cwd(), "src/routes/**/*.ts"), 
+      path.join(process.cwd(), "dist/routes/**/*.js"), 
+    ],
+  });
 };
